c784fcfb1947e6158e9203211d427872919a84e6
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-20 15:56:13 -0500

    Configurar input value en 0 para limpiar campo

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
 const btn = document.querySelector("[data-form-btn]");
 
-
-//Listener: escuchar un evento. //*Evento / Elemento que recibira el evento / lo que secedera cuando el evento sea realizado
-//objeto.addEventListener("evento", function (parametros) {lo que queremos que haga})
-
-
-btn.addEventListener("click", function (evento) {
+const createTask = (evento) => {
   evento.preventDefault();
-  const input = document.querySelector("[data-form-imput]");
-  //le indico que imprima el valor del input. 
-  console.log(input.value);
-});
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  input.value="";
+  console.log(value);
+};
+
+console.log(btn);
+
+btn.addEventListener("click", createTask);
