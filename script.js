e2748c2e4683d5e61547ec6644f0ab5e44c97fce
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-20 16:19:12 -0500

    Agregando codigo html al content

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ const createTask = (evento) => {
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
   //limpiar el input cuando se ingrese un dato.
+  const task = document.querySelector("[data-task]");
   input.value="";
-  console.log(value);
+
+  //*Indicamos que el contenido se muestre en la siguiente etiqueta de html introducciendo el valor. 
+
+  const content=
+ ` <div>
+  <i class="far fa-check-square icon"></i>
+  <span class="task">${value}</span>
+</div>
+<i class="fas fa-trash-alt trashIcon icon"></i>`
+
+//el value lo queremos insertar en una card, la cual tiene la variable task.
+//con la propiedad innerHTML anexamos codigo html.
+
+task.innerHTML = content;
+
+
+  console.log(content);
 };
 
 console.log(btn);
