9548a599c1e0eea3ee70b670bf6ad9f3d3f3433c
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-20 20:03:18 -0500

    Create element dinamicamente

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
-  evento.preventDefault();
+  evento.preventDefault(); //poner en blanco el campo de input
   const input = document.querySelector("[data-form-input]");
   const value = input.value;
-  //limpiar el input cuando se ingrese un dato.
-  const task = document.querySelector("[data-task]");
-  input.value="";
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li"); //creara un elemento
+  task.classList.add("card"); //darle clase al elmento creado.
+  input.value = " ";
 
-  //*Indicamos que el contenido se muestre en la siguiente etiqueta de html introducciendo el valor. 
+console.log(checkComplete());
+//creando elementos en orden
+  const taskContent = document.createElement('div');
 
-  const content=
- ` <div>
-  <i class="far fa-check-square icon"></i>
-  <span class="task">${value}</span>
-</div>
-<i class="fas fa-trash-alt trashIcon icon"></i>`
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
 
-//el value lo queremos insertar en una card, la cual tiene la variable task.
-//con la propiedad innerHTML anexamos codigo html.
 
-task.innerHTML = content;
+  const content = `
+<i class="fas fa-trash-alt trashIcon icon"></i>`;
 
+  //el value lo queremos insertar en una card, la cual tiene la variable task.
+  //con la propiedad innerHTML anexamos codigo html.
+
+  //! task.innerHTML = content;
+
+  task.appendChild(taskContent);
+
+
+  //agregar un hijo a un elemento padre
+
+  list.appendChild(task);
 
   console.log(content);
 };
@@ -29,3 +41,14 @@ task.innerHTML = content;
 console.log(btn);
 
 btn.addEventListener("click", createTask);
+
+//Hacer que el content viva dentro de una //*funcion.
+
+const checkComplete = () => {
+  const i = document.createElement("i");
+  i.classList.add("far");
+  i.classList.add("fa-check-square");
+  i.classList.add("icon");
+  
+  return i
+};
