329bbeea4497cab5868f4eb8b06ca5ba8473a9d5
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2023-03-20 23:12:15 -0500

    agregar modulos

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//!Para evitar que el usuario vea el codigo de JS se debe encerrar en una funcion. 
-//*immediately invoked function
-
-(() => {
+import checkComplete  from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
@@ -29,7 +27,7 @@ const createTask = (evento) => {
 
 
   task.appendChild(taskContent);
-  taskContent.appendChild(deleteIcon());
+  task.appendChild(deleteIcon());
   list.appendChild(task);
 
 };
@@ -38,39 +36,3 @@ btn.addEventListener("click", createTask); //Cuando se haga click se crea una ta
 
 //Hacer que el content viva dentro de una //*funcion.
 
-const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far","fa-check-square","icon");
-  addEventListener('click' , taskComplete);//cuando escuche el evento click haga la funcion de taskcomplete
-
-  return i
-};
-
-//*CONCLUIR TAREA
-
-const taskComplete = (event) => {
-const element = event.target;//encontrar el objetivo del elemento
-element.classList.toggle('fas');
-element.classList.toggle('completeIcon');
-element.classList.toggle('far'); //elimino una clase
-}
-// .remove es para remover // .toggle es para verificar si existe lo coloco si no no . 
-
-//*ELIMINAR TAREA
-
-const deleteIcon = () => {
-  const i = document.createElement("i");
-  i.classList.add("fas","fa-trash-alt","trashIcon","icon");
-  i.addEventListener('click',deleteTask);
-
-  return i;
-}
-
-const deleteTask = (event) => {
-
-  const parent = event.target.parentElement;//accedemos al evento, target y al elemento padre. 
-  console.log(parent);
-  parent.remove;
-}
-
-})();
